3af1f1768cadb2a7986a6bf69a59908badc60711
tanpreetjolly <tanpreet2002@gmail.com>
2024-02-20 15:43:37 +0530

    enhance ui and refactor blog component

--- client/src/components/BlogCard.tsx
+++ client/src/components/BlogCard.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -7,33 +6,42 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
 interface Blog {
-	title: string;
-	description: string;
-	img: string;
+  title: string;
+  description: string;
+  img: string;
+  author: string;
+  _id: string;
 }
 interface Props {
-	blog: Blog;
+  blog: Blog;
 }
 
 export default function MediaCard({ blog }: Props) {
-	return (
-		<Card sx={{ maxWidth: 345 }}>
-			<CardMedia sx={{ height: 140 }} image={blog.img} title={blog.title} />
-			<CardContent>
-				<Typography gutterBottom variant="h5" component="div">
-					{blog.title}
-				</Typography>
-				<Typography variant="body2" color="text.secondary">
-					{blog.description}
-				</Typography>
-				<Typography variant="body2" color="text.secondary">
-					Author: {blog.author}
-				</Typography>
-			</CardContent>
-			<CardActions>
-				<Button size="medium">Share</Button>
-				<Button size="medium">Read</Button>
-			</CardActions>
-		</Card>
-	);
+  return (
+    <Card sx={{ maxWidth: 300 }}>
+      <CardMedia sx={{ height: 200 }} image={blog.img} title={blog.title} />
+      <CardContent>
+        <div className="relative group hover:cursor-pointer">
+          <Typography gutterBottom variant="h5" className="">
+            {blog.title}
+          </Typography>
+          <div className="absolute h-0 group-hover:h-1  z-10 transition-all duration-300  bottom-0 left-0 w-full  bg-indigo-200 "></div>
+        </div>
+        <Typography>{blog.description}</Typography>
+        <Typography variant="body2"  className="!mt-3 text-indigo-600">
+          Author: {blog.author}
+        </Typography>
+      </CardContent>
+      <CardActions className="!px-4 my-2">
+        <button className=" rounded px-3 py-1.5  overflow-hidden group bg-indigo-500 relative hover:bg-gradient-to-r hover:from-indigo-500 hover:to-indigo-400 text-white hover:ring-1 hover:ring-offset-1 hover:ring-indigo-400 transition-all ease-out duration-300">
+          <span className="relative">
+            Read <span>&#8594;</span>{" "}
+          </span>
+        </button>
+        <button className=" rounded px-3 py-1.5  overflow-hidden group border-2 border-indigo-500 relative  text-indigo-600 hover:ring-1 hover:ring-offset-1 hover:ring-indigo-400 transition-all ease-out duration-300">
+          <span className="relative">Share</span>
+        </button>
+      </CardActions>
+    </Card>
+  );
 }
