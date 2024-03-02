797f42e311d5a3866612d7144eb35f291ad9334e
tanpreetjolly <tanpreet2002@gmail.com>
2024-03-03 00:37:50 +0530

    fix : router and layout issues

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,23 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { ThemeProvider } from "@mui/material/styles";
-import theme from "./theme.jsx";
+import theme from "./theme.tsx";
 
 import { Provider } from "react-redux";
 import store from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<BrowserRouter>
 			<ThemeProvider theme={theme}>
 				<Provider store={store}>
 					<App />
 				</Provider>
 			</ThemeProvider>
-		</BrowserRouter>
 		<Toaster />
 	</React.StrictMode>
 );
