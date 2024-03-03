20a3c69363134650cc9e2d26c7ab9a7514ef2764
tanpreetjolly <tanpreet2002@gmail.com>
2024-03-03 23:27:01 +0530

    change(ui): all blogs page and blogs card

--- client/src/Pages/AllBlogs.tsx
+++ client/src/Pages/AllBlogs.tsx
@@ -1,9 +1,53 @@
-type Props = {}
-
-const AllBlogs = (props: Props) => {
+import BlogPageNav from "../components/BlogPageNav";
+import BlogCard from "../components/BlogCard";
+import Categories from "../components/Categories";
+import { useState } from "react";
+const AllBlogs = () => {
+  const [category, setCategory] = useState<string>("For You");
   return (
-    <div>AllBlogs</div>
-  )
-}
+    <div>
+      <BlogPageNav />
+      <div className="flex gap-3 py-24 mx-auto w-5/6">
+        <div className=" w-3/4  ">
+          <Categories category={category} setCategory={setCategory} />
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+        </div>
+        <div className="w-[25%]  p-4 border rounded-lg mt-16 h-fit">
+          <h2 className="text-lg font-medium mb-5">
+            Trending{" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-5 h-5 inline mb-2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 18 9 11.25l4.306 4.306a11.95 11.95 0 0 1 5.814-5.518l2.74-1.22m0 0-5.94-2.281m5.94 2.28-2.28 5.941"
+              />
+            </svg>
+          </h2>
+          <div className="flex flex-col gap-1 mb-2 py-2 pb-3 border-t">
+            <p className="font-medium text-gray-700">JWT Authentication in NodeJS</p>
+            <div className="text-sm flex items-center gap-5 pr-5 text-gray-600"><span>John Doe</span> <span>500 reads</span></div>
+          </div>
+          <div className="flex flex-col gap-1 mb-2 py-2 pb-3 border-t">
+            <p className="font-medium text-gray-700">Cookies Authentication in NodeJS</p>
+            <div className="text-sm flex items-center gap-5 pr-5 text-gray-600"><span>John Doe</span> <span>750 reads</span></div>
+          </div>
+          <div className="flex flex-col gap-1 mb-2 py-2 pb-3 border-t">
+            <p className="font-medium text-gray-700">Bun a new NodeJS runtime</p>
+            <div className="text-sm flex items-center gap-5 pr-5 text-gray-600"><span>John Doe</span> <span>600 reads</span></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default AllBlogs
+export default AllBlogs;
--- client/src/components/BlogCard.tsx
+++ client/src/components/BlogCard.tsx
@@ -1,52 +1,58 @@
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-// import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
+const BlogCard = () => {
+  return (
+    <div className="container p-5    bg-white w-full border my-4 rounded-lg ">
+      <div className=" lg:flex lg:items-center  gap-3">
+        <img
+          className="object-cover w-4/5 mx-auto lg:mx-0 lg:w-52 aspect-square rounded-xl"
+          src="https://images.unsplash.com/photo-1590283603385-17ffb3a7f29f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+          alt=""
+        />
 
-import { BlogShortType } from "../definitions";
+        <div className="mt-6  lg:mt-0 lg:mx-6">
+          <div className="flex gap-1 items-center">
+            <p className="text-xs bg-highlight w-fit text-white py-1 px-2 rounded-full capitalize">
+              Dev
+            </p>
+            <p className="text-xs bg-highlight w-fit text-white py-1 px-2 rounded-full capitalize">
+              Finance
+            </p>
+          </div>
 
-import { useNavigate } from "react-router-dom";
+          <a
+            href="#"
+            className="block mt-3 text-2xl font-semibold text-gray-800 hover:underline md:text-3xl"
+          >
+            All the features you want to know
+          </a>
 
-interface Props {
-	blog: BlogShortType;
-}
+          <p className="mt-3 text-sm text-gray-500 md:text-sm">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+            veritatis sint autem nesciunt, laudantium quia tempore delect
+          </p>
 
-export default function MediaCard({ blog }: Props) {
-	const navigate = useNavigate();
-	return (
-		<Card sx={{ maxWidth: 300 }}>
-			<CardMedia sx={{ height: 200 }} image={blog.img} title={blog.title} />
-			<CardContent>
-				<div className="relative group hover:cursor-pointer">
-					<Typography gutterBottom variant="h5" className="">
-						{blog.title}
-					</Typography>
-					<div className="absolute h-0 group-hover:h-1  z-10 transition-all duration-300  bottom-0 left-0 w-full  bg-indigo-200 "></div>
-				</div>
-				<Typography>
-					{blog.description.length > 200
-						? `${blog.description.slice(0, 200)}...`
-						: blog.description}
-				</Typography>
-				<Typography variant="body2" className="!mt-3 text-indigo-600">
-					Author: {blog.author}
-				</Typography>
-			</CardContent>
-			<CardActions className="!px-4 my-2">
-				<button
-					className=" rounded px-3 py-1.5  overflow-hidden group bg-indigo-500 relative hover:bg-gradient-to-r hover:from-indigo-500 hover:to-indigo-400 text-white hover:ring-1 hover:ring-offset-1 hover:ring-indigo-400 transition-all ease-out duration-300"
-					onClick={() => navigate(`/blog/${blog._id}`)}
-				>
-					<span className="relative">
-						Read <span>&#8594;</span>{" "}
-					</span>
-				</button>
-				<button className=" rounded px-3 py-1.5  overflow-hidden group border-2 border-indigo-500 relative  text-indigo-600 hover:ring-1 hover:ring-offset-1 hover:ring-indigo-400 transition-all ease-out duration-300">
-					<span className="relative">Share</span>
-				</button>
-			</CardActions>
-		</Card>
-	);
-}
+          <a
+            href="#"
+            className="inline-block mt-2 text-blue-500 underline hover:text-blue-400"
+          >
+            Read more
+          </a>
+
+          <div className="flex items-center mt-6">
+            <img
+              className="object-cover object-center w-10 h-10 rounded-full"
+              src="https://images.unsplash.com/photo-1531590878845-12627191e687?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+              alt=""
+            />
+
+            <div className="mx-4">
+              <h1 className="text-sm text-gray-700">Amelia. Anderson</h1>
+              <p className="text-sm text-gray-500">Lead Developer</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogCard;
