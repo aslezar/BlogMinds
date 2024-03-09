a1ec8489219f1e63e4dc9ed5bd76e6e550d00c3d
Carbrex <95964955+Carbrex@users.noreply.github.com>
2024-03-09 14:31:13 +0530

    Added user seeder hehehehe

---
+++ DBseeder/userData.js
@@ -0,0 +1,17 @@
+const userData = [
+    {
+        name: "John Doe",
+        email: "johndoe@gmail.com",
+        password: "password",
+        bio: "I am a blogger.",
+        blogs: [],
+    },
+    {
+        name: "another user",
+        email: "anotheruser@gmail.com",
+        password: "password",
+        bio: "I am a another blogger.",
+        blogs: [],
+    },
+]
+module.exports = userData
