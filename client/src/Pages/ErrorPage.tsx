4fb493ee887e168a622b23c01b6d031911cb0fae
aslezar <97354675+aslezar@users.noreply.github.com>
2024-03-12 16:16:23 +0530

    corrected router link error page

--- client/src/Pages/ErrorPage.tsx
+++ client/src/Pages/ErrorPage.tsx
@@ -1,5 +1,4 @@
-import React from "react"
-
+import { Link } from "react-router-dom"
 const ErrorPage = () => {
   return (
     <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
@@ -12,15 +11,15 @@ const ErrorPage = () => {
           Sorry, we couldn’t find the page you’re looking for.
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
-          <a
-            href="#"
+          <Link
+            to="/"
             className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Go back home
-          </a>
-          <a href="#" className="text-sm font-semibold text-gray-900">
+          </Link>
+          <Link to="/contactus" className="text-sm font-semibold text-gray-900">
             Contact support <span aria-hidden="true">&rarr;</span>
-          </a>
+          </Link>
         </div>
       </div>
     </main>
