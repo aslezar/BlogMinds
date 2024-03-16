9eeffa97faabd6857384cf160171ebfcf460c9d0
aslezar <97354675+aslezar@users.noreply.github.com>
2024-03-16 16:02:21 +0530

    Update Cloudinary configuration using URL

--- utils/imageHandlers/cloudinary.ts
+++ utils/imageHandlers/cloudinary.ts
@@ -3,10 +3,11 @@ import { v2 as cloudinary } from "cloudinary"
 import sharp from "sharp"
 
 // configure cloudinary
+//config cloudinary using url
+// cloudinary.config({
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud: process.env.CLOUDINARY_URL,
+    secure: true,
 })
 
 // upload image to cloudinary and return the url
