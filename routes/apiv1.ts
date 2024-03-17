97cbe895b08a225603e8050bf052c757a5e720c3
aslezar <97354675+aslezar@users.noreply.github.com>
2024-03-17 23:50:38 +0530

    Add search functionality to API

--- routes/apiv1.ts
+++ routes/apiv1.ts
@@ -4,11 +4,13 @@ import AuthRouter from "./auth"
 import BlogRouter from "./blog"
 import UserRouter from "./user"
 import AIRouter from "./ai"
+import SearchRouter from "./search"
 
 const router = Router()
 
 router.use("/auth", AuthRouter)
 router.use("/blog", BlogRouter)
+router.use("/search", SearchRouter)
 router.use("/user", AuthMiddleware, UserRouter)
 router.use("/ai", AuthMiddleware, AIRouter)
 //localhost:5000/api/v1/ai
