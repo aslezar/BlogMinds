f710cac89851d6d65d0388c13ab8c3c3c16085f0
Carbrex <95964955+Carbrex@users.noreply.github.com>
2024-03-20 16:37:55 +0530

    Add GoogleSvg component to SignUpPage

--- client/src/Pages/SignUpPage.tsx
+++ client/src/Pages/SignUpPage.tsx
@@ -4,6 +4,7 @@ import { useAppDispatch, useAppSelector } from "../hooks"
 import img from "../assets/img/Auth/signup.webp"
 import { SignUpType } from "../definitions"
 import { register } from "../features/userSlice"
+import GoogleSvg from "../assets/img/Auth/GoogleSvg"
 
 export default function SignUp() {
   const dispatch = useAppDispatch()
@@ -41,6 +42,9 @@ export default function SignUp() {
   React.useEffect(() => {
     if (!loading && verificationRequired) {
       navigate("/verify")
+    }
+    else if (!loading && isAuthenticated) {
+      navigate(-1)// although this is not the best way to redirect, it is used here for simplicity
     }
   }, [loading, verificationRequired])
 
@@ -155,19 +159,7 @@ export default function SignUp() {
                 type="button"
                 className="w-full flex justify-center items-center gap-2 bg-highlight  text-white p-2.5 px-4 rounded-3xl hover:bg-dark border border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200 transition-colors duration-300 text-base font-medium"
               >
-                <svg
-                  className="w-5 h-5 text-gray-800 dark:text-white"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M12 22a10 10 0 0 1-7.1-3A9.9 9.9 0 0 1 5 4.8C7 3 9.5 2 12.2 2h.2c2.4 0 4.8 1 6.6 2.6l-2.5 2.3a6.2 6.2 0 0 0-4.2-1.6c-1.8 0-3.5.7-4.8 2a6.6 6.6 0 0 0-.1 9.3c1.2 1.3 2.9 2 4.7 2h.1a6 6 0 0 0 4-1.1c1-.9 1.8-2 2.1-3.4v-.2h-6v-3.4h9.6l.1 1.9c-.1 5.7-4 9.6-9.7 9.6H12Z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
+                <GoogleSvg />
                 Continue with Google
               </button>
             </div>
