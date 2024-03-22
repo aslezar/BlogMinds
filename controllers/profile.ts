040e056b06a1ab26b4eaa2d7b1a4489ec41402af
tanpreetjolly <tanpreet2002@gmail.com>
2024-03-22 23:41:50 +0530

    add blogs to response from profile controller

--- controllers/profile.ts
+++ controllers/profile.ts
@@ -1,4 +1,5 @@
 import User from "../models/user"
+import Blog from "../models/blog" // Import the Blog model
 import { Request, Response } from "express"
 import { StatusCodes } from "http-status-codes"
 import { BadRequestError } from "../errors"
@@ -18,21 +19,49 @@ const getUserProfile = async (req: Request, res: Response) => {
     const matchedUsers = await User.aggregate([
         { $match: { _id: getId(userId) } },
         {
+            $lookup: {
+                from: "blogs",
+                localField: "blogs",
+                foreignField: "_id",
+                as: "blogs",
+            },
+        },
+        {
             $project: {
                 name: 1,
                 bio: 1,
                 profileImage: 1,
-                blogs: { $size: "$blogs" },
                 followersCount: { $size: "$followers" },
                 followingCount: { $size: "$following" },
                 myInterests: 1,
+                createdAt: 1,
+                blogs: {
+                    $map: {
+                        input: "$blogs",
+                        as: "blog",
+                        in: {
+                            title: "$$blog.title",
+                            author: "$$blog.author",
+                            img: "$$blog.img",
+                            tags: "$$blog.tags",
+                            likesCount: "$$blog.likesCount",
+                            commentsCount: "$$blog.commentsCount",
+                            views: "$$blog.views",
+                            createdAt: "$$blog.createdAt",
+                            description : "$$blog.description"
+                        },
+                    },
+                },
             },
         },
     ])
+
     if (matchedUsers.length == 0) {
         throw new BadRequestError("User not found")
     }
+
     const user = matchedUsers[0]
+
     return res.status(StatusCodes.OK).json({
         data: {
             user,
@@ -41,5 +70,6 @@ const getUserProfile = async (req: Request, res: Response) => {
         msg: "User Fetched Successfully",
     })
 }
+
 
 export { getUserProfile }
