8d0fc02b42d67fcb031447bb0eb38c9b9bdbc3e2
Carbrex <95964955+Carbrex@users.noreply.github.com>
2024-03-23 19:01:41 +0530

    Add ImagePlaceholder component and replace SVG with ImagePlaceholder in BlogLoader component. Update loader in Blogs component to use BlogLoader component.

--- client/src/components/Blogs.tsx
+++ client/src/components/Blogs.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
 import { getBlogs, getRecommendedBlogs } from "../api/index"
 import { Category, BlogShortType, UserType } from "../definitions"
 import { useAppSelector } from "../hooks.tsx"
@@ -6,7 +6,7 @@ import InfiniteScroll from "react-infinite-scroll-component"
 
 import { useSearchParams } from "react-router-dom"
 import BlogCard from "./BlogCard"
-import ImagePlaceholder from "../assets/img/Feed/ImagePlaceholder.tsx"
+import BlogLoader from "./BlogLoader"
 
 const Blogs = () => {
   const [blogs, setBlogs] = useState<BlogShortType[]>([])
@@ -55,23 +55,7 @@ const Blogs = () => {
           next={() => fetchBlogs(user?.userId)}
           hasMore={hasMore}
           loader={
-            <div
-              role="status"
-              className=" pr-5 my-6 mr-8 animate-pulse md:space-y-0  rtl:space-x-reverse md:flex flex-row-reverse md:items-center justify-between"
-            >
-              <div className="flex items-center justify-center w-full h-40 mt-4 bg-gray-300 rounded-md sm:w-96 ">
-                <ImagePlaceholder />
-              </div>
-              <div className="w-full">
-                <div className="h-2.5 bg-gray-200 rounded-full w-48 mb-4"></div>
-                <div className="h-2 bg-gray-200 rounded-full max-w-[480px] mb-2.5"></div>
-                <div className="h-2 bg-gray-200 rounded-full mb-2.5 w-4/5"></div>
-                <div className="h-2 bg-gray-200 rounded-full max-w-[440px] mb-2.5"></div>
-                <div className="h-2 bg-gray-200 rounded-full max-w-[460px] mb-2.5"></div>
-                <div className="h-2 bg-gray-200 rounded-full max-w-[360px]"></div>
-              </div>
-              <span className="sr-only">Loading...</span>
-            </div>
+            Array.from({ length: 3 }).map((_, index) => <BlogLoader key={index} />)
           }
         >
           {blogs.map((blog, index) => (
