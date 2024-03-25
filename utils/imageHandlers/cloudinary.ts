6857234034979413c172e6b24444957092614080
aslezar <97354675+aslezar@users.noreply.github.com>
2024-03-26 01:29:35 +0530

    used cloudinary way to convert file to webp

--- utils/imageHandlers/cloudinary.ts
+++ utils/imageHandlers/cloudinary.ts
@@ -1,10 +1,6 @@
 import { Request } from "express"
 import { v2 as cloudinary } from "cloudinary"
-import sharp from "sharp"
 
-// configure cloudinary
-//config cloudinary using url
-// cloudinary.config({
 cloudinary.config({
     cloud: process.env.CLOUDINARY_URL,
     secure: true,
@@ -12,24 +8,13 @@ cloudinary.config({
 
 // upload image to cloudinary and return the url
 const uploadProfileImage = async (req: Request) => {
-    const webpImg = await sharp(req.file.buffer)
-        .webp({ quality: 80 })
-        .toBuffer()
-
-    //delete prev image if exists
-    // const prevImg = await cloudinary.search
-    //     .expression(`folder:blogmind/${req.user.userId}/profile`)
-    //     .execute()
-    // if (prevImg.resources.length > 0) {
-    //     await cloudinary.uploader.destroy(prevImg.resources[0].public_id)
-    // }
-
     const result = await cloudinary.uploader.upload(
-        `data:${req.file.mimetype};base64,${webpImg.toString("base64")}`,
+        `data:${req.file.mimetype};base64,${req.file.buffer.toString("base64")}`,
         {
             folder: "blogmind",
             public_id: `${req.user.userId}/profile`,
             overwrite: true,
+            format: "webp",
         },
     )
     return result.secure_url
@@ -40,16 +25,13 @@ const uploadAssetsImages = async (req: Request) => {
     const urls: string[] = []
 
     for (const file of files) {
-        const webpImg = await sharp(file.buffer)
-            .webp({ quality: 80 })
-            .toBuffer()
-
         const originalName = file.originalname.split(".")[0] // remove original extension
         const result = await cloudinary.uploader.upload(
-            `data:${file.mimetype};base64,${webpImg.toString("base64")}`,
+            `data:${file.mimetype};base64,${file.buffer.toString("base64")}`,
             {
                 folder: "blogmind",
                 public_id: `${req.user.userId}/${originalName}`,
+                format: "webp",
             },
         )
         urls.push(result.secure_url)
