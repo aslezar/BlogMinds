808cf58d2bc059831ed8293bf675feae75e0861f
aslezar <97354675+aslezar@users.noreply.github.com>
2024-03-26 01:30:26 +0530

    Added uuid to assest image, removed CDN cache images on overwrite

--- utils/imageHandlers/cloudinary.ts
+++ utils/imageHandlers/cloudinary.ts
@@ -1,6 +1,6 @@
 import { Request } from "express"
 import { v2 as cloudinary } from "cloudinary"
-
+import { v4 as uuidv4 } from "uuid"
 cloudinary.config({
     cloud: process.env.CLOUDINARY_URL,
     secure: true,
@@ -11,10 +11,11 @@ const uploadProfileImage = async (req: Request) => {
     const result = await cloudinary.uploader.upload(
         `data:${req.file.mimetype};base64,${req.file.buffer.toString("base64")}`,
         {
-            folder: "blogmind",
-            public_id: `${req.user.userId}/profile`,
+            folder: `blogmind/${req.user.userId}`,
+            public_id: "profile",
             overwrite: true,
             format: "webp",
+            invalidate: true,
         },
     )
     return result.secure_url
@@ -25,15 +26,17 @@ const uploadAssetsImages = async (req: Request) => {
     const urls: string[] = []
 
     for (const file of files) {
-        const originalName = file.originalname.split(".")[0] // remove original extension
+        const originalName = file.originalname.split(".")[0] + "_" + uuidv4()
         const result = await cloudinary.uploader.upload(
             `data:${file.mimetype};base64,${file.buffer.toString("base64")}`,
             {
-                folder: "blogmind",
-                public_id: `${req.user.userId}/${originalName}`,
+                folder: `blogmind/${req.user.userId}`,
+                public_id: originalName,
+                overwrite: false,
                 format: "webp",
             },
         )
+
         urls.push(result.secure_url)
     }
     return urls
