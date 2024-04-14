66a520b9b981fbed4afcd6a6faa27555b25d684c
tanpreetjolly <tanpreet2002@gmail.com>
2024-04-14 18:28:54 +0530

    change(dependencies): title and parser plugin in editorjs

--- client/src/components/Editor.tsx
+++ client/src/components/Editor.tsx
@@ -1,15 +1,34 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useRef, useState } from "react"
 import { EditorContext } from "../context/EditorContext"
+// Import the package
+import edjsHTML from "editorjs-html"
 
 const EditorPage = () => {
   const { initializeEditor, editorInstanceRef } = useContext(EditorContext)
+  const [data, setData] = useState<any>(null)
+  const editorRef = useRef<any>(null)
+  const handleClick = async () => {
+    const output = await editorInstanceRef.current.save()
+    const edjsParser = edjsHTML();
+    let html = edjsParser.parse(output);
+    console.log(html);
+    setData(html)
+  }
+  console.log(data)
   useEffect(() => {
-    if (editorInstanceRef.current == null) {
+    if (editorRef.current == null) {
       initializeEditor()
+      editorRef.current = true
     }
   }, [])
   // Match the id with holder value in EditorContext.tsx
-  return <div id="editorjs"></div>
+
+  return (
+    <div>
+      <button onClick={handleClick}>Save</button>
+      <div id="editorjs"></div>
+    </div>
+  )
 }
 
 export default EditorPage
