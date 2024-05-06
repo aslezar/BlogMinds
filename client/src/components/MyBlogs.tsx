e27d72932768104be4b45c4bdf83b2228603c996
Kshitij <kshitij.tanwar2002@gmail.com>
2024-05-07 00:40:35 +0530

    feat: Added blog card in MyBlogs

--- client/src/components/MyBlogs.tsx
+++ client/src/components/MyBlogs.tsx
@@ -1,95 +1,77 @@
-import React from "react"
-import { getUserBlogs, deleteBlog } from "../api"
+import { useEffect, useState } from "react"
+import { getUserBlogs } from "../api"
 import { BlogShortType } from "../definitions"
-import Loader from "./Loader"
-import { NavLink } from "react-router-dom"
+import { Link } from "react-router-dom"
+import Pagination from "@mui/material/Pagination"
 
 const MyBlogs = () => {
-  const [page, setPage] = React.useState<number>(1)
-  const [totalCount, setTotalCount] = React.useState<number>(0)
-  const [blogs, setBlogs] = React.useState<BlogShortType[]>([])
-  const [loading, setLoading] = React.useState<boolean>(true)
-  const [deleteLoading, setDeleteLoading] = React.useState<boolean>(false)
-  const limit = 10
+  const [page, setPage] = useState<number>(1)
+  const [blogs, setBlogs] = useState<BlogShortType[]>([])
+  const [totalCount, setTotalCount] = useState<number>(0)
+  const limit = 6
 
-  const totalPages = Math.ceil(totalCount / limit)
-
-  React.useEffect(() => {
-    setLoading(true)
+  useEffect(() => {
     getUserBlogs(page, limit)
       .then((res) => {
         setBlogs(res.data.blogs)
         setTotalCount(res.data.totalCount)
+        console.log(res.data)
       })
       .catch((err) => console.log(err))
-      .finally(() => setLoading(false))
   }, [page])
 
-  const nextPage = () => {
-    if (page < totalPages) setPage(page + 1)
-  }
-
-  const prevPage = () => {
-    if (page > 1) setPage(page - 1)
-  }
-
-  const handleDeleteBlog = (id: BlogShortType["_id"]) => {
-    if (
-      !window.confirm(
-        "Are you sure you want to delete this blog?\nThis Action can be revert back.",
-      )
-    )
-      return
-    setDeleteLoading(true)
-    deleteBlog(id)
-      .then((_res) => {
-        setBlogs((prev) => prev.filter((blog) => blog._id !== id))
-        setTotalCount((prev) => prev - 1)
-      })
-      .catch((err) => console.log(err))
-      .finally(() => setDeleteLoading(false))
-  }
-
   return (
-    <div>
-      {/* pagination */}
-      <div>
-        <button onClick={prevPage} disabled={page === 1}>
-          Previous
-        </button>
-        &nbsp; {/* non-breaking space */}
-        {Array.from({ length: totalPages }, (_, index) => (
-          <button
-            key={index + 1}
-            onClick={() => setPage(index + 1)}
-            style={{ fontWeight: page === index + 1 ? "bold" : "normal" }}
-          >
-            {index + 1}&nbsp;
-          </button>
-        ))}
-        <button onClick={nextPage}>Next</button>
-      </div>
-
-      {loading ? (
-        <Loader />
-      ) : blogs.length === 0 ? (
-        "No blogs found"
-      ) : (
-        blogs.map((blog, index) => (
-          <div key={blog._id}>
-            <NavLink to={`/blog/${blog._id}`}>
-              {index + 1}. {blog.title}
-            </NavLink>
-            <NavLink to={`/write/${blog._id}`}>Edit</NavLink>
-            <button
-              onClick={() => handleDeleteBlog(blog._id)}
-              disabled={deleteLoading}
-            >
-              Delete
-            </button>
-          </div>
-        ))
+    <div className="w-11/12 mx-auto flex flex-col justify-between h-[78vh]">
+      {blogs.length === 0 && (
+        <h1 className="text-3xl font-medium ">No blogs found</h1>
       )}
+      {!!blogs.length && (
+        <ul className="list-none grid grid-cols-3 gap-7">
+          {blogs.map((blog) => (
+            <Link
+              to={`/blog/${blog._id}`}
+              className="max-w-sm mx-auto flex flex-col gap-2 border-b py-4"
+            >
+              <h1 className="text-2xl font-bold text-gray-600 line-clamp-1">
+                {blog.title}
+              </h1>
+              <p className="width-auto  text-gray-600 flex-shrink line-clamp-2 h-14">
+                {blog.description}
+              </p>
+              <img
+                src={
+                  blog.img ||
+                  "https://i0.wp.com/www.bishoprook.com/wp-content/uploads/2021/05/placeholder-image-gray-16x9-1.png?ssl=1"
+                }
+                alt="img"
+                className="aspect-video rounded-md "
+                loading="lazy"
+              />
+              {/* render blogs.myInterests.map in a flex div */}
+              <div className="flex gap-2 items-center flex-wrap">
+                {blog.tags.map((tag) => (
+                  <span className="text-gray-600 bg-gray-200 px-2 py-1 rounded-full text-xs">
+                    {tag}
+                  </span>
+                ))}
+              </div>
+            </Link>
+          ))}
+        </ul>
+      )}
+
+      {/* pagination */}
+      <footer>
+        <Pagination
+          className="!my-6 !ml-auto !w-fit !mr-6"
+          count={Math.ceil(totalCount / limit)}
+          color="secondary"
+          shape="rounded"
+          onChange={(_event: React.ChangeEvent<unknown>, value: number) => {
+            setPage(value)
+          }}
+        />
+      </footer>
     </div>
   )
 }
