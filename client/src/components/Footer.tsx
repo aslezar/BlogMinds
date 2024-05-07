04e43dfa04098319bc93e1d1e6b1e43e5ac5da00
Kshitij <kshitij.tanwar2002@gmail.com>
2024-05-08 01:47:51 +0530

    fix: Interests UI fix in MyProfile

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -7,8 +7,8 @@ const Footer = () => {
     <footer className="shadow bg-gray-900">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <a
-            href="https://flowbite.com/"
+          <NavLink
+            to="https://blogminds.onrender.com/"
             className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
           >
             <svg
@@ -25,7 +25,7 @@ const Footer = () => {
             <span className="self-center text-2xl font-semibold whitespace-nowrap text-white tracking-wide uppercase">
               Creativerse
             </span>
-          </a>
+          </NavLink>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium sm:mb-0 text-gray-400">
             <li>
               <NavLink to="/about" className="hover:underline me-4 md:me-6">
