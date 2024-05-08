128d13d00b5ed397afb698d82190576d0da1c2b2
aslezar <97354675+aslezar@users.noreply.github.com>
2024-05-08 18:57:18 +0530

    Added posthog-react

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,16 +9,26 @@ import theme from "./theme.tsx"
 import { Provider } from "react-redux"
 import store from "./store"
 import { EditorContextProvider } from "./context/EditorContext.tsx"
+import { PostHogProvider } from "posthog-js/react"
+
+const options = {
+  api_host: import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_HOST,
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <EditorContextProvider>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </EditorContextProvider>
-      <Toaster />
-    </ThemeProvider>
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
+      <ThemeProvider theme={theme}>
+        <EditorContextProvider>
+          <Provider store={store}>
+            <App />
+          </Provider>
+        </EditorContextProvider>
+        <Toaster />
+      </ThemeProvider>
+    </PostHogProvider>
   </React.StrictMode>,
 )
