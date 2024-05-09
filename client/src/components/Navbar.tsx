e1eb6bf00c561489ebab210ccc65c8b30e7ca953
aslezar <97354675+aslezar@users.noreply.github.com>
2024-05-09 19:20:37 +0530

    Navbar button

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,6 +3,9 @@ import { useAppDispatch, useAppSelector } from "../hooks"
 import SearchBar from "./SearchBar"
 import { useEffect, useRef, useState } from "react"
 import { logout } from "../features/userSlice"
+import Button from "@mui/material/Button"
+import Menu from "@mui/material/Menu"
+import MenuItem from "@mui/material/MenuItem"
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -21,6 +24,10 @@ const Navbar = () => {
     )
     if (!res) return
     dispatch(logout())
+    setIsOpen(false)
+  }
+
+  const handleClose = () => {
     setIsOpen(false)
   }
   useEffect(() => {
@@ -158,105 +165,84 @@ const Navbar = () => {
                 </svg>
                 <span className=" text-white font-medium">Write</span>
               </NavLink>
-              <button
-                onClick={() => {
-                  setIsOpen(!isOpen)
-                }}
-                className="p-3 rounded-full bg-dark relative hover:bg-highlight duration-200"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="white"
-                  className="w-5 h-5 "
+
+              <div>
+                <button
+                  onClick={() => {
+                    setIsOpen(!isOpen)
+                  }}
+                  className="p-3 rounded-full bg-dark relative hover:bg-highlight duration-200"
                 >
-                  <path
-                    fillRule="evenodd"
-                    d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </button>
-              {isOpen && (
-                <div
-                  ref={dropdownRef}
-                  className="absolute z-10 top-20 right-[5%] mt-1 w-52 font-medium  text-gray-600 flex flex-col bg-white rounded-xl overflow-auto border border-gray-300 shadow-lg "
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="white"
+                    className="w-5 h-5 "
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
+                <Menu
+                  id="basic-menu"
+                  open={isOpen}
+                  onClose={handleClose}
+                  MenuListProps={{
+                    "aria-labelledby": "basic-button",
+                  }}
                 >
-                  <NavLink
-                    to={"/profile"}
-                    onClick={() => {
-                      setIsOpen(false)
-                    }}
-                    className={`px-6 py-4 cursor-pointer flex items-center gap-1 text-left hover:bg-highlight hover:text-white`}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-5 h-5 inline "
+                  <MenuItem onClick={handleClose}>
+                    <NavLink
+                      to={"/profile"}
+                      onClick={() => {
+                        setIsOpen(false)
+                      }}
+                      className={`px-6 py-4 cursor-pointer flex items-center gap-1 text-left hover:bg-highlight hover:text-white`}
                     >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
-                      />
-                    </svg>
-                    Profile
-                  </NavLink>
-                  <NavLink
-                    onClick={() => {
-                      setIsOpen(false)
-                    }}
-                    to={"/settings"}
-                    className={`px-6 py-4 flex items-center gap-1 cursor-pointer text-left hover:bg-highlight hover:text-white`}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-5 h-5"
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-5 h-5 inline "
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                        />
+                      </svg>
+                      Profile
+                    </NavLink>
+                  </MenuItem>
+                  <MenuItem onClick={handleClose}>
+                    <button
+                      onClick={handleLogout}
+                      className={`px-6 py-4 cursor-pointer text-left hover:bg-highlight hover:text-white flex items-center gap-1`}
                     >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z"
-                      />
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                      />
-                    </svg>
-                    Settings
-                  </NavLink>
-                  <button
-                    onClick={handleLogout}
-                    className={`px-6 py-4 cursor-pointer text-left hover:bg-highlight hover:text-white flex items-center gap-1`}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-5 h-5"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15"
-                      />
-                    </svg>
-                    Log Out
-                  </button>
-                  {/* other options like logout, settings and my blogs page should also exist */}
-                </div>
-              )}
-              {/* dropdown with navigate to profile option and should display only when isOpen true */}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-5 h-5"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15"
+                        />
+                      </svg>
+                      Log Out
+                    </button>
+                  </MenuItem>
+                </Menu>
+              </div>
             </div>
           )}
         </div>
