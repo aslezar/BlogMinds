1e0b3c26015d2efec9309741afcc12b8d8ce84dd
tanpreetjolly <tanpreet2002@gmail.com>
2024-05-11 13:49:27 +0530

    refactored ui in features section

--- client/src/components/Features.tsx
+++ client/src/components/Features.tsx
@@ -4,7 +4,7 @@ import imgGen from "../assets/videos/Features/imgGen.mp4"
 const Features: React.FC = () => {
   return (
     <div className="w-11/12 lg:w-5/6 mx-auto lg:mt-20 overflow-hidden pt-20" id="features">
-      <h1 className="text-3xl lg:text-6xl font-semibold text-center my-4 ">
+      <h1 className="text-4xl lg:text-6xl font-semibold text-center  ">
         Publish{' '}
         <span className="bg-gradient-to-r from-dark from-60% to-highlight text-transparent bg-clip-text">
           Effo
@@ -16,15 +16,15 @@ const Features: React.FC = () => {
           eslly
         </span>
       </h1>
-      <p className="text-center text-slate-500 text-lg">
+      <p className="text-center text-slate-500 text-lg mb-4">
         Write blogs effortlessly with AI-driven text suggestions and images.
       </p>
       <section className="flex flex-col w-full lg:my-10  md:flex-row items-center lg:gap-10 ">
         <div className="lg:w-5/6 p-2 max-w-lg mx-auto">
-          <h1 className="font-semibold p-2 text-2xl lg:text-3xl mb-2">
+          <h1 className="text-center md:tex-left font-semibold p-2 text-2xl lg:text-3xl mb-2">
             Drag and Drop Editor
           </h1>
-          <div className="flex gap-3 mb-3 min-h-40 ">
+          <div className="flex gap-3 mb-3 min-h-40 text-sm md:text-base">
             <div className="border p-4 rounded-lg bg-neutral-50">
               Simply Drag n Drop to Images to your blog.
             </div>
@@ -33,7 +33,7 @@ const Features: React.FC = () => {
               drag-and-drop a standout feature.
             </div>
           </div>
-          <div className="border p-4 rounded-lg  bg-neutral-50">
+          <div className="border p-4 rounded-lg  bg-neutral-50 text-sm md:text-base mb-2">
             A more interactive and modern experience while saving time and
             effort.
           </div>
@@ -52,15 +52,15 @@ const Features: React.FC = () => {
       </section>
       <section className="flex flex-col w-full my-14 lg:my-28 item-center md:flex-row-reverse items-center lg:gap-10">
         <div className="lg:w-5/6 p-2 max-w-lg mx-auto">
-          <h1 className="lg:text-right font-semibold p-2 text-2xl lg:text-3xl mb-2">
+          <h1 className="text-center lg:text-right font-semibold p-2 text-2xl lg:text-3xl mb-2">
             Generative AI to enhance image and text creation
           </h1>
           <div className="flex gap-3 mb-3 min-h-40 ">
-            <div className="border p-4 rounded-lg bg-neutral-50">
+            <div className="border p-4 rounded-lg bg-neutral-50 text-sm md:text-base">
               Enjoy an intuitive interface that simplifies the process of
               creating unique visuals effortlessly.
             </div>
-            <div className="border p-4 rounded-lg bg-neutral-50">
+            <div className="border p-4 rounded-lg bg-neutral-50 text-sm md:text-base">
               Image generation based on prompts!
             </div>
           </div>
